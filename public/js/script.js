d6bca13385ed02b6187870371d1cb3aec40aff13
MathieuDacheux <mathieu.dacheux@icloud.com>
2022-07-25 16:29:43 +0200

    Terminado

---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+// Apparition au défilement
+
+const threshold = .15;
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold
+}
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > threshold) {
+      entry.target.classList.remove('reveal')
+      observer.unobserve(entry.target)
+    }
+  })
+}
+
+document.documentElement.classList.add('reveal-loaded')
+
+window.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver(handleIntersect, options)
+  const targets = document.querySelectorAll('.reveal')
+  targets.forEach(function (target) {
+    observer.observe(target)
+  })
+})
